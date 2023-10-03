756b679802e34cb622e62053adbb7110ea056442
Emmanuel Wang <71987344+sturdybinder@users.noreply.github.com>
2023-10-03 13:02:39 -0700

    update scrollBehavior.js

--- js/scrollBehavior.js
+++ js/scrollBehavior.js
@@ -1,17 +1,58 @@
-function scrollToSection(id) {
-  event.preventDefault();
-  var element = document.getElementById(id);
-  element.scrollIntoView({ behavior: "smooth" });
+// Function to replay the animation
+function replayAnimation(element) {
+  // Remove the 'name-text' class to clear the animation
+  element.classList.remove("name-text");
 
-  // Remove active class from all links
-  var links = document.querySelectorAll(".navbarText");
-  links.forEach(link => {
-    link.classList.remove("active");
+  // Trigger a reflow to remove the animation
+  void element.offsetWidth;
+
+  // Add the 'name-text' class back to restart the animation
+  element.classList.add("name-text");
+}
+
+// Create an Intersection Observer
+// Create an Intersection Observer
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const element = entry.target;
+    if (entry.isIntersecting) {
+      // Element is in view, add the 'name-text' class
+      element.classList.add("name-text");
+      
+      // Reset animation properties
+      element.style.opacity = '0';
+      element.style.transform = 'translateY(20px)';
+      void element.offsetWidth;
+      element.style.animation = 'fade-in 1s 0.3s forwards, slide-up 1s 0.3s forwards';
+    } else {
+      // Element is out of view, remove the 'name-text' class
+      element.classList.remove("name-text");
+      
+      // Reset animation properties for fade-out
+      element.style.opacity = '1';
+      element.style.transform = 'translateY(0)';
+      void element.offsetWidth;
+      element.style.animation = 'fade-out 1s forwards, slide-down 1s forwards';
+    }
   });
+});
 
-  // Add active class to the clicked link
-  var clickedLink = document.querySelector(`[href="#${id}"]`);
-  clickedLink.classList.add("active");
+
+// Observe each 'name-text' element
+const nameTextElements = document.querySelectorAll(".name-text");
+nameTextElements.forEach(element => {
+  observer.observe(element);
+});
+
+// Call reloadAnimations on page load to initialize animations
+function reloadAnimations() {
+  nameTextElements.forEach(element => {
+    var rect = element.getBoundingClientRect();
+    if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+      // Element is in view, reload animations
+      replayAnimation(element);
+    }
+  });
 }
 
 // Add an event listener to track scroll and update active link
@@ -29,6 +70,9 @@ document.addEventListener("scroll", function () {
       activeLink.classList.add("active");
     }
   });
+
+  // Call the function to reload animations when scrolling
+  reloadAnimations();
 });
 
 // Call the initializePage function when the page loads
@@ -40,7 +84,9 @@ function initializePage() {
 }
 
 window.addEventListener("load", initializePage);
-window.scrollToSection = function(id) {
+
+// Modified scrollToSection to include the replayAnimations call
+function scrollToSection(id) {
   event.preventDefault();
   var element = document.getElementById(id);
   element.scrollIntoView({ behavior: "smooth" });
@@ -54,4 +100,7 @@ window.scrollToSection = function(id) {
   // Add active class to the clicked link
   var clickedLink = document.querySelector(`[href="#${id}"]`);
   clickedLink.classList.add("active");
-};
+
+  // Call the function to reload animations when scrolling
+  reloadAnimations();
+}
