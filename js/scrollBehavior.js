abc499a56e68f53265b97f646caa877f03c12e08
Emmanuel Wang <71987344+sturdybinder@users.noreply.github.com>
2023-10-12 19:31:03 -0700

    update scrollBehavior.js

--- js/scrollBehavior.js
+++ js/scrollBehavior.js
@@ -1,133 +1,86 @@
 // Function to replay the animation
 function replayAnimation(element) {
-  // Remove the 'name-text' class to clear the animation
   element.classList.remove("name-text");
-
-  // Trigger a reflow to remove the animation
   void element.offsetWidth;
-
-  // Add the 'name-text' class back to restart the animation
   element.classList.add("name-text");
 }
 
-// Create an Intersection Observer
-// Create an Intersection Observer
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     const element = entry.target;
-    if (entry.isIntersecting) {
-      // Element is in view, add the 'name-text' class
-      element.classList.add("name-text");
-      
-      // Reset animation properties
-      element.style.opacity = '0';
-      element.style.transform = 'translateY(20px)';
-      void element.offsetWidth;
-      element.style.animation = 'fade-in 1s 0.3s forwards, slide-up 1s 0.3s forwards';
-    } else {
-      // Element is out of view, remove the 'name-text' class
-      element.classList.remove("name-text");
-      
-      // Reset animation properties for fade-out
-      element.style.opacity = '1';
-      element.style.transform = 'translateY(0)';
-      void element.offsetWidth;
-      element.style.animation = 'fade-out 1s forwards, slide-down 1s forwards';
-    }
+    element.classList.toggle("name-text", entry.isIntersecting);
+    element.style.opacity = entry.isIntersecting ? '0' : '1';
+    element.style.transform = entry.isIntersecting ? 'translateY(20px)' : 'translateY(0)';
+    void element.offsetWidth;
+    element.style.animation = entry.isIntersecting
+      ? 'fade-in 1s 0.3s forwards, slide-up 1s 0.3s forwards'
+      : 'fade-out 1s forwards, slide-down 1s forwards';
   });
 });
 
+document.querySelectorAll(".name-text").forEach(element => observer.observe(element));
 
-// Observe each 'name-text' element
-const nameTextElements = document.querySelectorAll(".name-text");
-nameTextElements.forEach(element => {
-  observer.observe(element);
-});
-
-// Call reloadAnimations on page load to initialize animations
 function reloadAnimations() {
-  nameTextElements.forEach(element => {
-    var rect = element.getBoundingClientRect();
+  document.querySelectorAll(".name-text").forEach(element => {
+    const rect = element.getBoundingClientRect();
     if (rect.top <= window.innerHeight && rect.bottom >= 0) {
-      // Element is in view, reload animations
       replayAnimation(element);
     }
   });
 }
 
-// Add an event listener to track scroll and update active link
-document.addEventListener("scroll", function () {
-  var sections = document.querySelectorAll("section");
-  sections.forEach(section => {
-    var rect = section.getBoundingClientRect();
+document.addEventListener("scroll", () => {
+  document.querySelectorAll("section").forEach(section => {
+    const rect = section.getBoundingClientRect();
     if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
-      var sectionId = section.id;
-      var activeLink = document.querySelector(`[href="#${sectionId}"]`);
-      var links = document.querySelectorAll(".nav-text");
-      links.forEach(link => {
-        link.classList.remove("active");
-      });
-      activeLink.classList.add("active");
+      document.querySelectorAll(".nav-text").forEach(link => link.classList.remove("active"));
+      document.querySelector(`[href="#${section.id}"]`).classList.add("active");
     }
   });
-
-  // Call the function to reload animations when scrolling
   reloadAnimations();
 });
 
-// Call the initializePage function when the page loads
 function initializePage() {
-  var firstSection = document.querySelector("section");
-  var firstLinkId = firstSection.id;
-  var firstLink = document.querySelector(`[href="#${firstLinkId}"]`);
-  firstLink.classList.add("active");
+  const firstSection = document.querySelector("section");
+  const firstLinkId = firstSection.id;
+  document.querySelector(`[href="#${firstLinkId}"]`).classList.add("active");
 }
 
 window.addEventListener("load", initializePage);
 
-// Modified scrollToSection to include the replayAnimations call
 function scrollToSection(id) {
   event.preventDefault();
-  var element = document.getElementById(id);
+  const element = document.getElementById(id);
   element.scrollIntoView({ behavior: "smooth" });
-
-  // Remove active class from all links
-  var links = document.querySelectorAll(".nav-text");
-  links.forEach(link => {
-    link.classList.remove("active");
-  });
-
-  // Add active class to the clicked link
-  var clickedLink = document.querySelector(`[href="#${id}"]`);
-  clickedLink.classList.add("active");
-
-  // Call the function to reload animations when scrolling
+  document.querySelectorAll(".nav-text").forEach(link => link.classList.remove("active"));
+  document.querySelector(`[href="#${id}"]`).classList.add("active");
   reloadAnimations();
 }
 
 const inkContainer = document.getElementById('ink-container');
 let lastDotTime = 0;
-const dotCooldown = 25; // Set the cooldown time in milliseconds
+const dotCooldown = 25;
 
-document.addEventListener('mousemove', (e) => {
-    const currentTime = Date.now();
-    if (currentTime - lastDotTime > dotCooldown) {
-        createInkDot(e.clientX, e.clientY);
-        lastDotTime = currentTime;
-    }
+document.addEventListener('mousemove', e => {
+  const currentTime = Date.now();
+  if (currentTime - lastDotTime > dotCooldown) {
+    createInkDot(e.clientX, e.clientY);
+    lastDotTime = currentTime;
+  }
 });
 
+const navContainer = document.querySelector('.nav-container');
+
+navContainer.addEventListener('mouseenter', () => clearTimeout(inkDotTimeout));
+
+navContainer.addEventListener('mouseleave', () => createInkDot(x, y));
+
+let inkDotTimeout;
+
 function createInkDot(x, y) {
-    const inkDot = document.createElement('div');
-    inkDot.classList.add('ink-dot');
-    inkDot.style.left = x + 'px';
-    inkDot.style.top = y + 'px';
-    inkContainer.appendChild(inkDot);
-
-    // Remove the dot after a delay
-    setTimeout(() => {
-        inkContainer.removeChild(inkDot);
-    }, 10000); // Adjust the delay (in milliseconds) for fading away
+  const inkDot = document.createElement('div');
+  inkDot.className = 'ink-dot';
+  inkDot.style.cssText = `left: ${x}px; top: ${y}px;`;
+  inkContainer.appendChild(inkDot);
+  inkDotTimeout = setTimeout(() => inkContainer.removeChild(inkDot), 10000);
 }
-
-
