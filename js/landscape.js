2f27a43243d759cc8330499fef3cfb635595b429
eleisian <emmanueltwang@gmail.com>
2023-11-15 20:39:25 -0800

    fun animation, still figuring it out

--- js/landscape.js
+++ js/landscape.js
@@ -1,69 +1,80 @@
-const canvas = document.getElementById("landscape");
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
+// Set up scene
+var scene = new THREE.Scene();
 
-// Set clear color to transparent
-renderer.setClearColor(0xffffff, 0);
+// Set up camera
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 50;
 
-const dots = [];
-const maxDots = 2000;
-const initialDotsCount = 400;
-const initialRadius = 0.008;
-const phi = (1 + Math.sqrt(5)) / 2;
+// Set up renderer with alpha: true for a clear background
+var renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('landscape'), alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-const createDot = () => {
-    if (dots.length < maxDots) {
-        const i = dots.length + 1;
-        const theta = 2 * Math.PI * i / phi;
-        const radius = initialRadius * Math.sqrt(i);
-        const x = radius * Math.cos(theta);
-        const y = radius * Math.sin(theta);
-        const z = 0;
+// Function to create a planet with its orbit
+function createPlanet(radius, color, orbitRadiusX, orbitRadiusY, orbitSegments, orbitSpeed, zAngle) {
+  // Orbit
+  var orbitGeometry = new THREE.EllipseCurve(0, 0, orbitRadiusX, orbitRadiusY, 0, 2 * Math.PI, false, zAngle);
+  var orbitPoints = orbitGeometry.getPoints(orbitSegments);
+  var orbitMaterial = new THREE.LineBasicMaterial({ color: 0x000000, transparent: true, opacity: 0.5 });
 
-        const dotGeometry = new THREE.SphereGeometry(0.005, 8, 8);
-        const dotMaterial = new THREE.MeshBasicMaterial({ color: 0xffaf1e });
-        const dot = new THREE.Mesh(dotGeometry, dotMaterial);
-        dot.position.set(x, y, z);
-        scene.add(dot);
-        dots.push(dot);
-    } else {
-        dots[dots.length % maxDots].position.set(0, 0, 0);
-    }
-};
+  var orbit = new THREE.Line(new THREE.BufferGeometry().setFromPoints(orbitPoints), orbitMaterial);
+  scene.add(orbit);
 
-const handleWindowResize = () => {
-    const { clientWidth, clientHeight } = canvas;
-    
-    // Update the camera's aspect ratio
-    camera.aspect = clientWidth / clientHeight;
-    camera.updateProjectionMatrix();
+  // Planet
+  var planetGeometry = new THREE.CircleGeometry(radius, 32);
+  var planetMaterial = new THREE.MeshBasicMaterial({ color: color, side: THREE.DoubleSide });
 
-    // Set the renderer's size to match the CSS size
-    renderer.setSize(clientWidth, clientHeight);
-};
+  var planet = new THREE.Mesh(planetGeometry, planetMaterial);
+  scene.add(planet);
 
-window.addEventListener("resize", handleWindowResize);
-
-// Set initial camera position and look at the scene center
-camera.position.set(0, 0, 1);
-camera.lookAt(0, 0, 0);
-
-for (let i = 1; i <= initialDotsCount; i++) {
-    createDot();
+  return { orbit: orbit, planet: planet, orbitRadiusX: orbitRadiusX, orbitRadiusY: orbitRadiusY, angle: 0, orbitSpeed: orbitSpeed, zAngle: zAngle };
 }
 
-const animate = () => {
-    requestAnimationFrame(animate);
-    createDot();
+// Create individual planets with orbits
+var planets = [
+  createPlanet(0.2, 0x00FF00, 3, 2, 64, 0.01, 0.1),
+  createPlanet(0.3, 0xFF0000, 5, 4, 64, 0.008, 0.2),
+  createPlanet(0.25, 0x0000FF, 7, 6, 64, 0.005, 0.3),
+  createPlanet(0.35, 0xFFFF00, 9, 8, 64, 0.003, 0.4),
+  createPlanet(0.18, 0xFF00FF, 11, 10, 64, 0.002, 0.5)
+];
 
-    dots.forEach((dot) => {
-        dot.visible = true;
-    });
+// Set up animation
+function animate() {
+  requestAnimationFrame(animate);
 
-    renderer.render(scene, camera);
-    scene.rotation.z += 0.0005;
-};
+  // Rotate the planets around the sun
+  planets.forEach((planet) => {
+    planet.angle += planet.orbitSpeed; // Adjusted to ensure different speeds
 
-handleWindowResize();
+    // Calculate the position based on the ellipse equation
+    var x = planet.orbitRadiusX * Math.cos(planet.angle);
+    var y = planet.orbitRadiusY * Math.sin(planet.angle);
+
+    // Apply rotation to the orbit
+    planet.orbit.rotation.x += 0.01;
+    planet.orbit.rotation.y += 0.01;
+    planet.orbit.rotation.z += 0.01;
+
+    // Set the position of the planet along the ellipse
+    planet.planet.position.x = x;
+    planet.planet.position.y = y;
+
+    // Reset z position to zero
+    planet.planet.position.z = 0;
+  });
+
+  renderer.render(scene, camera);
+}
+
+// Initial setup
+planets.forEach((planet) => {
+  // Randomize initial rotations for orbits
+  planet.orbit.rotation.x = Math.random() * Math.PI * 2;
+  planet.orbit.rotation.y = Math.random() * Math.PI * 2;
+  planet.orbit.rotation.z = Math.random() * Math.PI * 2;
+
+  scene.add(planet.orbit);
+  scene.add(planet.planet);
+});
+
 animate();
