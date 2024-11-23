e8dcca977281a01120a8bea5bad7f94bf79eb16c
e. wang <71987344+eleisian@users.noreply.github.com>
2024-11-23 01:23:40 -0800

    update

--- blog/blogPosts.js
+++ blog/blogPosts.js
@@ -16,10 +16,5 @@ export const blogPosts = [
         content: `I'm learning to be kinder to myself. I'm learning to be kinder to others. I'm learning to be kinder to the world.
         I'm learning to be kinder to the future. I'm trying to be aware of a presence that is greater than myself. I'm trying to not just talk alone in the dark. 
         I want others to be able to hear my heart and not my words.`
-    },
-    {
-        title: "The state of the silicon valley tech industry",
-        date: "2024-11-13",
-        content: `<blockquote class="twitter-tweet"><p lang="en" dir="ltr">This industry is so fundamentally unserious <br><br>I should have been a plumber <a href="https://t.co/yCjs3dVkIB">https://t.co/yCjs3dVkIB</a></p>&mdash; tuna🍣 (@tunahorse21) <a href="https://twitter.com/tunahorse21/status/1857575409560277244?ref_src=twsrc%5Etfw">November 16, 2024</a></blockquote>`
     }
 ]; 
