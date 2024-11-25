381f46f280a588f5b85e89c2e5d6b4f0ce744e45
e. wang <71987344+eleisian@users.noreply.github.com>
2024-11-24 20:58:26 -0800

    die

--- blog/blogPosts.js
+++ blog/blogPosts.js
@@ -1,20 +1,2 @@
 export const blogPosts = [
-    {
-        title: "one hundred more, ten thousand more",
-        date: "2024-11-10",
-        content: `Всё гениальное просто!`
-
-    },
-    {
-        title: "captain of the ship",
-        date: "2024-11-11",
-        content: `I am the captain of the ship.`
-    },
-    {
-        title: "until the two hearts become one",
-        date: "2024-11-12",
-        content: `I'm learning to be kinder to myself. I'm learning to be kinder to others. I'm learning to be kinder to the world.
-        I'm learning to be kinder to the future. I'm trying to be aware of a presence that is greater than myself. I'm trying to not just talk alone in the dark. 
-        I want others to be able to hear my heart and not my words.`
-    }
 ]; 
